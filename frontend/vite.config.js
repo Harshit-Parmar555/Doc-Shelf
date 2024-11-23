47d2d7c5e43e2224dbe12b523b6fbca1742a181e
Harshit_Parmar <parmarharshit441@gmail.com>
2024-11-23 15:39:15 +0530

    fix bugs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:6060",
+      "/api": "https://doc-shelf.onrender.com",
     },
   },
   plugins: [react()],
