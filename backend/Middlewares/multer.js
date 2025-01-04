4acd00796d9ea72c1493f0fe719d7a872f676898
Harshit_Parmar <parmarharshit441@gmail.com>
2025-01-04 20:29:17 +0530

    Fix Public Folder Bug

--- backend/Middlewares/multer.js
+++ backend/Middlewares/multer.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./public");
+    cb(null, "backend/public");
   },
   filename: (req, file, cb) => {
     const uniquename = Date.now() + "_" + file.originalname;
